59bc3ed33faf45042283845a36024e15dc42af87
BorisKolganov <bkolganov@me.com>
2017-02-09 00:04:53 +0300

    admin fix

--- adverts/admin.py
+++ adverts/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from models import Advert, Category, SubCategory
 
-admin.site.register(Advert)
+
 admin.site.register(Category)
 admin.site.register(SubCategory)
+
+
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ('header', 'owner', 'is_moderated', 'is_active', 'is_sold', 'is_best')
+    list_editable = ('is_moderated', 'is_active', 'is_sold', 'is_best')
+    search_fields = ('header', 'text', 'owner')
+    list_filter = ('is_moderated', 'is_active', 'is_sold', 'is_best')
+
+admin.site.register(Advert, AdvertAdmin)
